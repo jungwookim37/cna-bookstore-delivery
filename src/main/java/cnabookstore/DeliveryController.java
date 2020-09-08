f25d04a806c19c0243d5003d2acce7309686a67d
jungwookim37 <68408656+jungwookim37@users.noreply.github.com>
2020-09-08 21:25:45 +0900

    Update DeliveryController.java

--- src/main/java/cnabookstore/DeliveryController.java
+++ src/main/java/cnabookstore/DeliveryController.java
@@ -1,10 +1,10 @@
 package cnabookstore;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -12,4 +12,26 @@ import java.util.List;
  @RestController
  public class DeliveryController {
 
+  @GetMapping("/circuitBreaker")
+  @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500"),
+          @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000"),
+          @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),
+          @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+          @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")
+  })
+  public String circuitBreakerTest(@RequestParam String isYn) {
+
+   if (isYn.equals("Y")) {
+    System.out.println("@@@ CircuitBreaker!!!");
+    throw new RuntimeException("CircuitBreaker!!!");
+   }
+
+   return isYn;
+  }
+
+  private String fallback(String isYn) {
+   System.out.println("### fallback!!!");
+   return "CircuitBreaker!!!";
+  }
  }
